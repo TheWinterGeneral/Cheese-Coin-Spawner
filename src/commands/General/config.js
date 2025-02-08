00c91c55108f0c90480b7a95ed443c6e9c338951
TheWinterGeneral <ArloJ09@hotmail.com>
2025-02-08 16:16:26 -0700

    Create config.js

---
+++ src/commands/General/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("config")
+    .setDescription("Configure the messages required and time to spawn coins")
+    .addNumberOption((option) =>
+      option
+        .setName("time")
+        .setDescription("The time that the bot takes to reset")
+        .setRequired(true),
+    )
+    .addNumberOption((option) =>
+      option
+        .setName("messages")
+        .setDescription("The number of messages required")
+        .setRequired(true),
+    ),
+
+  run: ({ interaction, client, handler }) => {
+    global.messages = interaction.options.getString("messages");
+    global.time = interaction.options.getString("time");
+
+    interaction.reply(`Done ${global.time} ${global.messages}`);
+  },
+};
